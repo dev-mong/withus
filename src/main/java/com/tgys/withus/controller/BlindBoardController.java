e80c96dedea4a0c04b7bbd28d5f5d0de86f49a19
MJguri <pmy20804@gmail.com>
2023-02-14 01:10:17 +0900

    setting basic code

--- src/main/java/com/tgys/withus/controller/BlindBoardController.java
+++ src/main/java/com/tgys/withus/controller/BlindBoardController.java
@@ -1,4 +1,20 @@
 package com.tgys.withus.controller;
 
+import com.tgys.withus.domain.BlindBoard;
+import com.tgys.withus.service.BlindService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class BlindBoardController {
+    private final BlindService blindService;
+
+    @GetMapping("/test")
+    public List<BlindBoard> testController(){
+        return blindService.findAll();
+    }
 }
